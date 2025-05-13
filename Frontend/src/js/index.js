22f96fa9e0622c76659e75b5fc510651ae942152
Matthew Cania <matthewgab24@gmail.com>
2025-05-13 21:28:38 +0800

    jquery email and password value getters

--- Frontend/src/js/index.js
+++ Frontend/src/js/index.js
@@ -1,9 +1,7 @@
+$(document).ready(function () {
+  $("#submit_btn").click(function () {
+	const email = $("#email").val();
+	const password = $("#password").val()
 
-$(function() {
-
-	
-
+  });
 });
-$(".testBtn").click(function(){
-	$(".test").text("World")
-})
